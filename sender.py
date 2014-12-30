f859fc7aa0cc4e3cacdc770f70c1847ddc61ec54
ugotnitined <nitin.sadras@gmail.com>
2014-12-30 12:24:00 -0800

    colorschemes + questionable error handling

--- sender.py
+++ sender.py
@@ -7,6 +7,7 @@ class StripTest(BaseStripAnim):
         #The base class MUST be initialized by calling super like this
         super(StripTest, self).__init__(led, start, end)
         #Create a color array to use in the animation
+        christmas = [colors.Red, colors.Green, colors.White]
         rainbow = [colors.Red, colors.Orange, colors.Yellow, colors.Green, colors.Blue, colors.Indigo, colors.Violet]
         ocean = [colors.DarkBlue, colors.Blue, colors.Teal, colors.Turquoise]
         fire = [colors.Red, colors.Orange, colors.DarkOrange, colors.OrangeRed, colors.DarkRed]
@@ -15,8 +16,11 @@ class StripTest(BaseStripAnim):
         tron = [(0xDF, 0x74, 0xC), (0x6F, 0xC3, 0xDF), (0x0C, 0x14, 0x1F)]
         dark = [colors.DarkCyan, colors.DarkBlue, colors.DarkGreen]
         periwinkle = [colors.SlateBlue]
-        sparse = [colors.Black, colors.Black, colors.Black, colors.Black, colors.Black, colors.Black, colors.Black, colors.DarkBlue]
-        self._colors = sparse
+        test = [colors.DarkBlue]
+        sparse = [colors.Black, colors.Black, colors.Black, colors.Black, colors.Black, colors.Black, colors.Black, colors.Black, colors.Black, colors.DarkBlue]
+        purple = [colors.Purple, colors.Violet, colors.DarkViolet]
+        off = [colors.Black]
+        self._colors = fire 
 
     def step(self, amt = 1):
         #Fill the strip, with each sucessive color 
@@ -27,8 +31,14 @@ class StripTest(BaseStripAnim):
 
 
 #must init with same number of pixels as receiver
-driver = DriverNetwork(45, host = "192.168.0.105")
+driver = DriverNetwork(45, host = "192.168.1.15")
 led = LEDStrip(driver)
 
 anim = StripTest(led)
-anim.run(sleep=500)
+while True:
+    try:
+        anim.run(sleep=500)
+    except KeyboardInterrupt:
+        break
+    except:
+        pass
